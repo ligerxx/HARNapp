c296f4528a6d7ca2cee28dd6e4e8c27afc5d6b11
Casey Feldman <ligerxx@me.com>
2013-02-13 12:53:07 -0500

    Added CameraViewController .h & .m files to start implementing camera integration

---
+++ HARN Xcode/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  HARN
+//
+//  Created by Casey Feldman on 2/13/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end
