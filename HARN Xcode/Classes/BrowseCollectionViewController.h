743d509c63fa6a5121f83ab8399db2c2873749ce
Casey Feldman <ligerxx@me.com>
2013-02-21 14:34:44 -0500

    Started some foundation work for our collection views
    Collection View reiquires iOS 6+. not a big deal but it'll be easier

---
+++ HARN Xcode/Classes/BrowseCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  BrowseCollectionViewController.h
+//  HARN
+//
+//  Created by Casey Feldman on 2/21/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BrowseCollectionViewController : UICollectionViewController
+
+@end
---
+++ HARN Xcode/Classes/BrowseCollectionViewController.m
@@ -0,0 +1,38 @@
+//
+//  BrowseCollectionViewController.m
+//  HARN
+//
+//  Created by Casey Feldman on 2/21/13.
+//
+//
+
+#import "BrowseCollectionViewController.h"
+
+@interface BrowseCollectionViewController ()
+
+@end
+
+@implementation BrowseCollectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
