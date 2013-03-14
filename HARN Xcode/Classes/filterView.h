5a070365cbbd499adb568305eaaf26a9e7710892
Casey Feldman <ligerxx@me.com>
2013-03-14 19:19:58 -0400

    Working on separating aspects of the camera UI into various components

---
+++ HARN Xcode/Classes/filterView.h
@@ -0,0 +1,13 @@
+//
+//  filterView.h
+//  HARN
+//
+//  Created by Casey Feldman on 3/14/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface filterView : UIView
+
+@end
---
+++ HARN Xcode/Classes/filterView.m
@@ -0,0 +1,31 @@
+//
+//  filterView.m
+//  HARN
+//
+//  Created by Casey Feldman on 3/14/13.
+//
+//
+
+#import "filterView.h"
+
+@implementation filterView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
