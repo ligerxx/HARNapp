6ff3d1833e83b20cb0e343a4882a87b5f10ac51e
Casey Feldman <ligerxx@me.com>
2013-04-11 16:56:10 -0400

    Made an Artwork Information Class that will hold all the information for every collection.
    It is added in the collection view controller as a property and being
    generated at ViewDidLoad. The conditions right now are for asian only
    but even though the conditions are met it doesnt seem to be generating
    anything and is nil

---
+++ HARN Xcode/Classes/ArtworkInformation.h
@@ -0,0 +1,13 @@
+//
+//  ArtworkInformation.h
+//  HARN
+//
+//  Created by Casey Feldman on 4/11/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ArtworkInformation : NSObject
+
+@end
---
+++ HARN Xcode/Classes/ArtworkInformation.m
@@ -0,0 +1,13 @@
+//
+//  ArtworkInformation.m
+//  HARN
+//
+//  Created by Casey Feldman on 4/11/13.
+//
+//
+
+#import "ArtworkInformation.h"
+
+@implementation ArtworkInformation
+
+@end
